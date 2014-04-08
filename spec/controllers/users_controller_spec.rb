0c7ebd2335445caa6d1bed34f7f70c49bb749dcf
Leo Tumwattana <leo.tumwattana@gmail.com>
2014-04-08 19:49:39 +0800

    Adds Users json API with tests

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe UsersController do
+
+  render_views
+
+  before :each do
+    @user = User.create(email: 'kulio@gmail.com', password: '1234', password_confirmation: '1234')
+  end
+
+  describe 'GET index' do
+    it 'returns a list of users' do
+      get :index, :format => :json
+      expect(response.status).to eq 200
+      expect(JSON.load(response.body)["users"][0]["email"]).to eq 'kulio@gmail.com'
+    end
+  end
+
+  describe 'GET show' do
+    it 'returns one user' do
+      get :show, id: @user.id, :format => :json
+      expect(response.status).to eq 200
+      expect(JSON.load(response.body)["email"]).to eq 'kulio@gmail.com'
+
+    end
+  end
+
+end
