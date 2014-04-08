1441b931c420286e948be6f3231465bd43430254
Leo Tumwattana <leo.tumwattana@gmail.com>
2014-04-08 22:41:33 +0800

    Refactors task_spec.rb

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,25 +2,24 @@ require 'spec_helper'
 
 describe Task do
 
+  before :each do
+    @task = Task.new
+  end
+
   it { should belong_to(:event) }
 
   it 'defaults to uncompleted' do
-    @task = Task.new
     expect(@task.completed).to eq(false)
-
   end
 
   it 'is invalid without a name' do
-    @task = Task.new
     expect(@task).to be_invalid
     expect(@task).to have(1).error_on(:name)
   end
 
   it 'is valid with a name' do
-    @task = Task.new
     @task.name = "not empty"
     expect(@task).to be_valid
-
   end
 
 end
