f396080c2619af70cf111895cd4a6f033b167f1a
Leo Tumwattana <leo.tumwattana@gmail.com>
2014-04-08 22:46:29 +0800

    Adds validates numericality integer_only to stat model & test

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -2,6 +2,8 @@ class Stat < ActiveRecord::Base
 
   after_initialize :set_defaults
 
+  validates :points, numericality: { only_integer: true }
+
   belongs_to :user
 
   def set_defaults
--- spec/models/stat_spec.rb
+++ spec/models/stat_spec.rb
@@ -2,11 +2,17 @@ require 'spec_helper'
 
 describe Stat do
 
+  describe "associations" do
+    it { should belong_to(:user) }
+  end
+
+  describe "validations" do
+    it { should validate_numericality_of :points }
+  end
+
   it "has default points of 0" do
     stat = Stat.new
     expect(stat.points).to eq 0
   end
-
-  it { should belong_to(:user) }
 
 end
