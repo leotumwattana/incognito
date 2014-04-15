1dd0b7d9a76a4923440cdb79142dd9dc7e54f639
miteymouse <chenghtmark@gmail.com>
2014-04-15 15:38:38 +0800

    Adds calendar, marks events in calendar, lists events below calendar

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,11 @@
 class EventsController < ApplicationController
   def index
     @events = Event.all
-    @events_by_date = @events.group_by(&:date_time)
+    @events_by_date = @events.group_by { |event| event.date_time.to_date }
+    # binding.pry
+    # @events_by_date[@events.first.date_time.day]
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
+
   end
 
   def show
--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -4,7 +4,7 @@ module CalendarHelper
   end
 
   class Calendar < Struct.new(:view, :date, :callback)
-    HEADER = %w[Sunday Monday Tuesday Wednesday Thursday Friday Saturday]
+    HEADER = %w[SUN MON TUE WED THU FRI SAT]
     START_DAY = :sunday
 
     delegate :content_tag, to: :view
