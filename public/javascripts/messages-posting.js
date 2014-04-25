da035175a58a22bb40d3f34f3dfe17608abf8a76
Leo Tumwattana <leo.tumwattana@gmail.com>
2014-04-25 11:26:55 +0800

    Got Faye working!

--- public/javascripts/messages-posting.js
+++ public/javascripts/messages-posting.js
@@ -13,34 +13,35 @@ $(document).ready(
 
     scrollMessagesToBottom();
 
+    var getEventId = function() {
+      var intRegex = /[0-9 -()+]+$/;
+      return $(location).attr('hash').match(intRegex)[0];
+    }
+
+    var renderMessage = function(message) {
+      html = '<div class="message">' +
+        '<div class="user">' + message.user_id + '</div>' +
+        '<div class="content">' + message.content + '</div>' +
+      '</div>';
+      return html;
+    }
+
+    var appendMessage = function(html) { $('.messages').append(html); }
+
+    var clearTextArea = function() { $('#message').val(''); }
+
+    // Faye subscription
+    var channel = '/events/' + getEventId() + '/messages';
+    var faye = new Faye.Client('http://localhost:9292/faye');
+
+    faye.subscribe(channel, function (data) {
+      messageView = renderMessage(data);
+      appendMessage(messageView);
+      scrollMessagesToBottom(true);
+    })
+
     // click handler
     $('#post-button').on('click', function(e) {
-
-      var renderMessage = function(message) {
-        html = '<div class="message">' +
-          '<div class="user">' + message.user_id + '</div>' +
-          '<div class="content">' + message.content + '</div>' +
-        '</div>';
-        return html;
-      }
-
-      var appendMessage = function(html) {
-        $('.messages').append(html);
-      }
-
-      var clearTextArea = function() {
-        // $('#message').overlay([
-          // {
-            // match: /\B#([\w.])+/g,
-            // css: {'background-color': '#fff'}
-          // }]);
-        $('#message').val('');
-      }
-
-      var getEventId = function() {
-        var intRegex = /[0-9 -()+]+$/;
-        return $(location).attr('hash').match(intRegex)[0];
-      }
 
       var url = 'http://localhost:3000/api/events/' + getEventId() + '/messages';
 
@@ -50,10 +51,8 @@ $(document).ready(
         dataType: 'json',
         data: $('form').serialize(),
         success: function(data) {
-          messageView = renderMessage(data);
-          appendMessage(messageView);
-          scrollMessagesToBottom(true);
           clearTextArea();
+          faye.publish(channel, data);
         },
         error: function(x,y,z) {
           alert('Error posting message');
--- public/javascripts/messages.js
+++
@@ -1,7 +0,0 @@
-
-$(function() {
-  var faye = new Faye.Client('http://localhost:9292/faye');
-  faye.subscribe('/events/1/messages', function (data) {
-    alert(data);
-  });
-});
