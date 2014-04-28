902901fee951b1ad71be998a590bae6dea0ff31f
Leo Tumwattana <leo.tumwattana@gmail.com>
2014-04-28 10:53:33 +0800

    Adds extra seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,10 +14,13 @@ Message.destroy_all
 @mark = User.create({ username: "Mark", email: "mark@gmail.com", password: "123", password_confirmation: "123" })
 @nigel = User.create({ username: "Nigel", email: "nigel@gmail.com", password: "123", password_confirmation: "123" })
 
-@junk_trip = Event.create({title: "Junk Trip", date_time: "2014-05-15 13:00:00"})
-Event.create({title: "Some other event", date_time: "2014-05-20 10:00:00"})
-Event.create({title: "Thailand Trip", date_time: "2014-05-9 16:00:00"})
-Event.create({title: "WDI Seafood Trip", date_time: "2014-05-17 18:00:00"})
+@junk_trip = Event.create({title: "Junk Trip", date_time: "2014-04-28 13:00:00"})
+Event.create({title: "Some other event", date_time: "2014-04-29 10:00:00"})
+Event.create({title: "Thailand Trip", date_time: "2014-04-29 16:00:00"})
+Event.create({title: "WDI Seafood Trip", date_time: "2014-04-30 18:00:00"})
+Event.create({title: "Some other event", date_time: "2014-05-29 10:00:00"})
+Event.create({title: "Thailand Trip", date_time: "2014-05-29 16:00:00"})
+Event.create({title: "WDI Seafood Trip", date_time: "2014-05-30 18:00:00"})
 
 Message.create({event_id: @junk_trip.id, user_id: @kulio.id, content: "Let's go on a junk trip!"})
 Message.create({event_id: @junk_trip.id, user_id: @nigel.id, content: "Sounds good!"})
